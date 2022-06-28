04659fcb6e01ac6535125d366e374ae51e9beca0
Quinn R Davis <quinn.r.davis@gmail.com>
2022-06-28 16:34:16 -0700

    JavaScript Project 1

---
+++ Basic JavaScript Projects/Project1_expressions_alert/js/main.js
@@ -0,0 +1,9 @@
+// initialize string variables
+var sent1 = "This is the beginning of the string";
+var sent2 = " and this is the end of the string";
+
+// write concatenated strings
+document.write(sent1 + sent2);
+
+// create alert with expression
+window.alert(4 + 7);
